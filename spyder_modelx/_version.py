bfe878c9f0fdd27f85252d55b30190585055d62b
Fumito Hamamura <fumito.ham@gmail.com>
2019-01-13 12:29:57 +0900

    DIST: Release v0.0.8

--- spyder_modelx/_version.py
+++ spyder_modelx/_version.py
@@ -14,5 +14,5 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-version_info = (0, 0, 8, 'dev')
+version_info = (0, 0, 8)
 __version__ = '.'.join(map(str, version_info))
