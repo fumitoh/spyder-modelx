2121b59fc0ac26837575c5192b652973c2810ccb
Fumito Hamamura <fumito.ham@gmail.com>
2019-03-24 18:35:31 +0900

    DIST: Bump version back to dev

--- spyder_modelx/_version.py
+++ spyder_modelx/_version.py
@@ -14,5 +14,5 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-version_info = (0, 0, 9)
+version_info = (0, 0, 10, "dev")
 __version__ = '.'.join(map(str, version_info))
