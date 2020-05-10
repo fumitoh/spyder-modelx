16f2076a101c858a1e5ec479cb760317a77c28b1
Fumito Hamamura <fumito.ham@gmail.com>
2020-05-10 17:30:00 +0900

    DIST: Release v0.4.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def get_description():
     return data
 
 
-REQUIREMENTS = ['spyder>=3.2.5', 'modelx>=0.3.0', 'asttokens']
+REQUIREMENTS = ['spyder>=3.2.5', 'modelx>=0.6.0', 'asttokens']
 
 
 setup(
