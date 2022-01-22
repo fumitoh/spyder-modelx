7e0b2882ed850febcbfc902a989ad74bab290ee5
Fumito Hamamura <fumito.ham@gmail.com>
2022-01-22 16:28:51 +0900

    DOC: Update copyright year

--- spyder_modelx/plugins/mxplugin.py
+++ spyder_modelx/plugins/mxplugin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2018-2019 Fumito Hamamura <fumito.ham@gmail.com>
+# Copyright (c) 2018-2022 Fumito Hamamura <fumito.ham@gmail.com>
 
 # This library is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
