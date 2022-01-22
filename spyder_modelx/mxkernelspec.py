f96a0dcce9095c9600473e4255d0495436449d78
Fumito Hamamura <fumito.ham@gmail.com>
2022-01-22 16:24:14 +0900

    FIX: Reflect SpyderKernelSpec version diffs

--- spyder_modelx/mxkernelspec.py
+++ spyder_modelx/mxkernelspec.py
@@ -42,28 +42,114 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import os.path
+import sys
+import os
+import os.path as osp
 
 import spyder
-if spyder.version_info < (4,):
-    from spyder.utils.ipython.kernelspec import SpyderKernelSpec
-else:
-    from spyder.plugins.ipythonconsole.utils.kernelspec import SpyderKernelSpec
+from spyder.plugins.ipythonconsole.utils.kernelspec import SpyderKernelSpec
+from spyder.utils.misc import get_python_executable
+from spyder.utils.programs import is_python_interpreter
+
+if spyder.version_info < (5,):
+    from spyder.config.manager import CONF
+
+if spyder.version_info > (4, 1):    # 4.1.0 onwards
+    from spyder.plugins.ipythonconsole.utils.kernelspec import get_activation_script
+    import logging
+    logger = logging.getLogger(__name__)
+    from spyder.utils.conda import (add_quotes, get_conda_activation_script,
+                                    get_conda_env_path, is_conda_env)
+
+if spyder.version_info >= (4, 1, 1):  # 4.1.1 onwards
+    from spyder.plugins.ipythonconsole.utils.kernelspec import is_different_interpreter
+
 
 class MxKernelSpec(SpyderKernelSpec):
 
+    CONF_SECTION = 'ipython_console'    # 5.1.0 onwards
+
     @property
     def argv(self):
-
-        kernel_cmd = SpyderKernelSpec.argv.fget(self)
-
-        if spyder.version_info < (3, 3, 0):
-            here = os.path.abspath(os.path.dirname(__file__))
-            kernel_cmd[1] = os.path.join("%s" % here, "start_kernel.py")
+        """Command to start kernels"""
+        # Python interpreter used to start kernels
+        if spyder.version_info < (5, 1):
+            if CONF.get('main_interpreter', 'default'):
+                pyexec = get_python_executable()
+            else:
+                if spyder.version_info < (4, 2):
+                    # Avoid IPython adding the virtualenv on which Spyder is running
+                    # to the kernel sys.path
+                    os.environ.pop('VIRTUAL_ENV', None)
+                pyexec = CONF.get('main_interpreter', 'executable')
+                if not is_python_interpreter(pyexec):
+                    pyexec = get_python_executable()
+                    CONF.set('main_interpreter', 'executable', '')
+                    CONF.set('main_interpreter', 'default', True)
+                    CONF.set('main_interpreter', 'custom', False)
         else:
-            kernel_cmd[2] = 'spyder_modelx.start_kernel'
+            if self.get_conf('default', section='main_interpreter'):
+                pyexec = get_python_executable()
+            else:
+                pyexec = self.get_conf('executable', section='main_interpreter')
+                if not is_python_interpreter(pyexec):
+                    pyexec = get_python_executable()
+                    self.set_conf('executable', '', section='main_interpreter')
+                    self.set_conf('default', True, section='main_interpreter')
+                    self.set_conf('custom', False, section='main_interpreter')
+
+        if spyder.version_info <= (4, 1, 1):
+            # Fixes spyder-ide/spyder#3427.
+            if os.name == 'nt':
+                dir_pyexec = osp.dirname(pyexec)
+                pyexec_w = osp.join(dir_pyexec, 'pythonw.exe')
+                if osp.isfile(pyexec_w):
+                    pyexec = pyexec_w
+
+        # Command used to start kernels
+        if spyder.version_info < (4, 1):    # 4.0.x
+            kernel_cmd = [
+                pyexec,
+                '-m',
+                'spyder_modelx.start_kernel',
+                '-f',
+                '{connection_file}'
+            ]
+        else:
+            if spyder.version_info == (4, 1, 1):
+                is_different = False    # pyexec != sys.executable
+            else:
+                # Part of spyder-ide/spyder#11819
+                is_different = is_different_interpreter(pyexec)
+
+            if is_different and is_conda_env(pyexec=pyexec):
+                # If this is a conda environment we need to call an intermediate
+                # activation script to correctly activate the spyder-kernel
+                # If changes are needed on this section make sure you also update
+                # the activation scripts at spyder/plugins/ipythonconsole/scripts/
+
+                if spyder.version_info < (4, 2):
+                    conda_activation_script = get_conda_activation_script()
+                else:
+                    conda_activation_script = get_conda_activation_script(pyexec)
+
+                kernel_cmd = [
+                    get_activation_script(),  # This is bundled with Spyder
+                    conda_activation_script,
+                    get_conda_env_path(pyexec),  # Might be external
+                    pyexec,
+                    '{connection_file}',
+                ]
+            else:
+                kernel_cmd = [
+                    pyexec,
+                    '-m',
+                    'spyder_modelx.start_kernel',
+                    '-f',
+                    '{connection_file}'
+                ]
+            logger.info('Kernel command: {}'.format(kernel_cmd))
 
         return kernel_cmd
-
 
 
